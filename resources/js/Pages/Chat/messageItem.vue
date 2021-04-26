a9ad5c765329e37100fcbb02c1dc7d0aeea1f292
kzubrzycki <kriszub98@gmail.com>
2021-04-26 15:08:47 +0200

    Showing user messages

--- resources/js/Pages/Chat/messageItem.vue
+++ resources/js/Pages/Chat/messageItem.vue
@@ -1,10 +1,9 @@
 <template>
-    <div>Message Item</div>
-    
+  <div>{{ message.user.name }}: {{ message.message }}</div>
 </template>
 
 <script>
-    export default{
-
-    }
+export default {
+  props: ["message"],
+};
 </script>
