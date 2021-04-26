e9d12b04d5be986e24e5d65b74c8ef598a2899af
kzubrzycki <kriszub98@gmail.com>
2021-04-26 11:39:43 +0200

    Chat controller, model change, routes

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ChatRoom;
+use App\Models\ChatMessage;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request)
+    {
+        return ChatRoom::all();
+    }
+
+    public function messages(Request $request, ChatRoom $chat_room) 
+    {
+        return ChatMessage()::where('chat_room_id', $chat_room->id)
+            ->with('user')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+    }
+
+    public function newMessage(Request $request, ChatRoom $chat_room) {
+        $newMessage = new ChatMessage;
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $chat_room->id;
+        $newMessage->message = $request->message;
+
+        $newMessage->save();
+        return $newMessage;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,12 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/chat', function () {
+    return Inertia::render('Chat/container');
+})->name('chat');
+
+Route::middleware('auth::sanctum')->get('/chat/rooms', [ChatController::class, 'rooms']);
+Route::middleware('auth::sanctum')->get('/chat/room/{chat_room}/messages', [ChatController::class, 'messages']);
+Route::middleware('auth::sanctum')->post('/chat/room/{chat_room}/message', [ChatController::class, 'newMessage']);
+
